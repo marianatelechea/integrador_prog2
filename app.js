85c1335739d896d6f52df428468a36fd9837efe9
Mili <mscipioni@udesa.edu.ar>
2020-05-18 11:45:40 -0300

    cambio en el app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var seriesRouter = require('./routes/series');
+var seriesRouter = require('./routes/paginas');
 
 var app = express();
 
