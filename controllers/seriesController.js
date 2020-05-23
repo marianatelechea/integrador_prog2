3ce6ee8d731590af072964e6d57ee8301db76e11
Mili <mscipioni@udesa.edu.ar>
2020-05-23 16:45:01 -0300

    elimine la consulta con la DB

--- controllers/seriesController.js
+++ controllers/seriesController.js
@@ -9,21 +9,7 @@ module.exports = {
 
     registro:(req, res) => { return res.render('registro'); },
 
-    pagina1: (req, res) => { 
-        db.Usuario
-        .findAll()
-        .then(series => {
-            return res.render ('inicio', {
-                listaSeries:series
-            });
-        })
-        .catch ((error) =>{
-            return res.send (error);
-        })
-    }, 
-        
-        
-       // return res.render('inicio'); },
+    pagina1: (req, res) => { return res.render ('inicio'); }, 
 
     pagina2: (req, res) => { return res.render('generos'); },
 
